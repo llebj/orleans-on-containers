5d5cc7fa965524ed73a0e39729713edbca5064ca
Joe Bell <35937399+llebj@users.noreply.github.com>
2023-11-18 12:26:43 +0000

    Created grain interface

---
+++ src/OrleansOnContainers/GrainInterfaces/IChatGrain.cs
@@ -0,0 +1,12 @@
+﻿using Orleans;
+
+namespace GrainInterfaces;
+
+public interface IChatGrain : IGrainWithIntegerKey
+{
+    Task Join(Guid clientId);
+
+    Task Leave(Guid clientId);
+
+    Task Message(string message);
+}
