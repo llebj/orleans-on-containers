9e599cf76b5fc6b7381d2afc499708a110d8c0cb
Joe Bell <35937399+llebj@users.noreply.github.com>
2023-11-18 12:40:25 +0000

    Implemented grain

---
+++ src/OrleansOnContainers/Grains/ChatGrain.cs
@@ -0,0 +1,37 @@
+﻿using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+
+namespace Grains;
+
+public class ChatGrain : Grain, IChatGrain
+{
+    private readonly ILogger<ChatGrain> _logger;
+
+    public ChatGrain(
+        ILogger<ChatGrain> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Join(Guid clientId)
+    {
+        _logger.LogInformation("Client {ClientId} joined chat {GrainId}", clientId, IdentityString);
+
+        return Task.CompletedTask;
+    }
+
+    public Task Leave(Guid clientId)
+    {
+        _logger.LogInformation("Client {ClientId} left chat {GrainId}", clientId, IdentityString);
+
+        return Task.CompletedTask;
+    }
+
+    public Task Message(string message)
+    {
+        _logger.LogInformation("Client {ClientId} says {Message}", message);
+
+        return Task.CompletedTask;
+    }
+}
