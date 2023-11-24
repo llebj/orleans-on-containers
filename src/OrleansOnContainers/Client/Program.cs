a043bda89abb24dfa9d7d4389b7ebecf3d72c0fb
Joe Bell <35937399+llebj@users.noreply.github.com>
2023-11-24 16:48:20 +0000

    Created Client

---
+++ src/OrleansOnContainers/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+Console.WriteLine("Hello, World!");
+
+var builder = Host.CreateDefaultBuilder(args)
+    .UseOrleansClient(client =>
+    {
+        client.UseLocalhostClustering();
+    })
+    .ConfigureLogging(logging => logging.AddConsole())
+    .UseConsoleLifetime();
+
+using IHost host = builder.Build();
+
+await host.RunAsync();
