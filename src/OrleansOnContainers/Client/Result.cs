5ddbe037cb0547f3880ba5b735a9c1e2456a0a29
Joe Bell <35937399+llebj@users.noreply.github.com>
2023-12-16 15:48:59 +0000

    Created result class

---
+++ src/OrleansOnContainers/Client/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace Client;
+
+public class Result
+{
+    public Result(bool isSuccess)
+    {
+        IsSuccess = isSuccess;
+        Message = string.Empty;
+    }
+
+    public Result(bool isSuccess, string message)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+    }
+
+    public bool IsSuccess { get; }
+
+    public string Message { get; }
+
+    public static Result Failure(string message) => new(false, message);
+
+    public static Result Success() => new(true);
+}
