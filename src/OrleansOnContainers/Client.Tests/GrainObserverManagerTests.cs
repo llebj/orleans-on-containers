cc137b7493c84304ec4335343d09a11f04217785
Joe Bell <35937399+llebj@users.noreply.github.com>
2023-12-18 11:12:52 +0000

    Injected IOptions into GrainObserverManager

--- src/OrleansOnContainers/Client.Tests/GrainObserverManagerTests.cs
+++ src/OrleansOnContainers/Client.Tests/GrainObserverManagerTests.cs
@@ -1,26 +1,36 @@
-﻿using Client.Services;
+﻿using Client.Options;
+using Client.Services;
 using GrainInterfaces;
+using Microsoft.Extensions.Options;
 using NSubstitute;
 using Xunit;
 
 namespace Client.Tests;
 
-public class GrainObserverManagerTests
+public class GrainObserverManagerTests : IClassFixture<GrainObserverManagerTestsFixture>
 {
+    private readonly GrainObserverManagerTestsFixture _fixture;
+
+    public GrainObserverManagerTests(GrainObserverManagerTestsFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
     [Fact]
     public async Task GivenNoActiveSubscription_WhenAClientAttemptsToSubscribe_ThenRegisterTheSubscriptionWithTheGrain()
     {
         // Arrange
+        var grainId = "test";
         var clusterClient = Substitute.For<IClusterClient>();
         var grain = Substitute.For<IChatGrain>();
         clusterClient
-            .GetGrain<IChatGrain>(Arg.Any<string>())
+            .GetGrain<IChatGrain>(grainId)
             .Returns(grain);
         var observer = Substitute.For<IChatObserver>();
-        var manager = new GrainObserverManager(clusterClient);
+        var manager = new GrainObserverManager(clusterClient, _fixture.ObserverManagerOptions);
 
         // Act
-        await manager.Subscribe(observer, "test");
+        await manager.Subscribe(observer, grainId);
 
         // Assert
         await grain.Received().Subscribe(observer);
@@ -36,11 +46,60 @@ public class GrainObserverManagerTests
             .GetGrain<IChatGrain>(Arg.Any<string>())
             .Returns(grain);
         var observer = Substitute.For<IChatObserver>();
-        var manager = new GrainObserverManager(clusterClient);
+        var manager = new GrainObserverManager(clusterClient, _fixture.ObserverManagerOptions);
         await manager.Subscribe(observer, "test");
 
         // Act
         // Assert
         await Assert.ThrowsAsync<InvalidOperationException>(() => manager.Subscribe(observer, "test-2"));
+    }
+
+    [Fact]
+    public async Task GivenAnActiveSubscription_WhenTheRefreshPeriodExpires_ThenResubscibeToTheSameGrain()
+    {
+        // Arrange
+        var grainId = "test";
+        var clusterClient = Substitute.For<IClusterClient>();
+        var grain = Substitute.For<IChatGrain>();
+        clusterClient
+            .GetGrain<IChatGrain>(grainId)
+            .Returns(grain);
+        var observer = Substitute.For<IChatObserver>();
+        var manager = new GrainObserverManager(clusterClient, _fixture.ObserverManagerOptions);
+        await manager.Subscribe(observer, grainId);
+
+        // Act
+
+        // Assert
+        clusterClient
+            .Received(2)
+            .GetGrain<IChatGrain>(grainId);
+        await grain
+            .Received(2)
+            .Subscribe(observer);
+    }
+}
+
+public class GrainObserverManagerTestsFixture 
+{
+    public GrainObserverManagerTestsFixture()
+    {
+        ObserverManagerOptions = MockOptions();
+    }
+
+    public IOptions<ObserverManagerOptions> ObserverManagerOptions { get; }
+
+    public int RefreshPariod { get; } = 150;
+
+    private IOptions<ObserverManagerOptions> MockOptions()
+    {
+        var substitute = Substitute.For<IOptions<ObserverManagerOptions>>();
+        var options = new ObserverManagerOptions
+        {
+            RefreshPeriod = RefreshPariod
+        };
+        substitute.Value.Returns(options);
+
+        return substitute;
     }
 }
--- src/OrleansOnContainers/Client/Services/GrainObserverManager.cs
+++ src/OrleansOnContainers/Client/Services/GrainObserverManager.cs
@@ -1,16 +1,21 @@
-﻿using GrainInterfaces;
+﻿using Client.Options;
+using GrainInterfaces;
+using Microsoft.Extensions.Options;
 
 namespace Client.Services;
 
 public class GrainObserverManager : IGrainObserverManager
 {
     private readonly IClusterClient _clusterClient;
+    private readonly IOptions<ObserverManagerOptions> _options;
     private bool _isSubscribed = false;
 
     public GrainObserverManager(
-        IClusterClient clusterClient)
+        IClusterClient clusterClient,
+        IOptions<ObserverManagerOptions> options)
     {
         _clusterClient = clusterClient;
+        _options = options;
     }
 
     public async Task Subscribe(IChatObserver observer, string grainId)
@@ -22,6 +27,9 @@ public class GrainObserverManager : IGrainObserverManager
             throw new InvalidOperationException(message);
         }
 
+        // A new PeriodicTimer wants to be instantiated and set to resubscribe
+        // on each timer tick. A cancellation token source wants to be created
+        // and passed into the timer, allowing the operation to be cancelled.
         var grain = _clusterClient.GetGrain<IChatGrain>(grainId);
         await grain.Subscribe(observer);
         _isSubscribed = true;
@@ -29,6 +37,9 @@ public class GrainObserverManager : IGrainObserverManager
 
     public Task Unsubscribe(IChatObserver observer, string grainId)
     {
+        // When a client unsubscibes, the re-subscribe operation wants to be cancelled
+        // using the cancellation token. Immediately afterwards, the existing timer wants
+        // to be disposed of and set to null.
         throw new NotImplementedException();
     }
 }
