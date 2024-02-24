b5a3b409c06da68aa7d8f41f006fe90be64ec3b8
Joe Bell <35937399+llebj@users.noreply.github.com>
2024-02-24 17:40:57 +0000

    Created test class for new chat grain

---
+++ src/OrleansOnContainers/Grains.Tests/ChatGrainV2Tests.cs
@@ -0,0 +1,12 @@
+﻿namespace Grains.Tests;
+
+public class ChatGrainV2Tests
+{
+    // GivenASubscribedObserver_WhenAMessageIsSentToTheGrain_ThenTheObserverReceivesTheMessage
+
+    // GivenASubscribedObserver_WhenAnotherObserverSubscribes_ThenNotifyTheExistingObserver
+
+    // GivenMultipleSubscribedObservers_WhenOneObserverUnsubscribes_ThenNotifyTheRemainingObserer
+
+    // GivenAnObserverThatHasUnsubscribed_WhenAMessageIsSentToTheGrain_ThenTheObserverDoesNotReceiveAnyMoreMessages
+}
---
+++ src/OrleansOnContainers/Grains/ChatGrainV2.cs
@@ -0,0 +1,21 @@
+﻿using GrainInterfaces;
+
+namespace Grains;
+
+public class ChatGrainV2 : Grain, IChatGrain
+{
+    public Task SendMessage(Guid clientId, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Subscribe(IChatObserver observer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Unsubscribe(IChatObserver observer)
+    {
+        throw new NotImplementedException();
+    }
+}
