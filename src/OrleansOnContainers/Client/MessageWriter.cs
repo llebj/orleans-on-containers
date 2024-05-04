db3801ffaa508f07e6168b6e66d5d90b846bc87b
Joe Bell <35937399+llebj@users.noreply.github.com>
2024-05-04 10:03:53 +0100

    Added timestamp to client messages.

--- src/OrleansOnContainers/Client/MessageWriter.cs
+++ src/OrleansOnContainers/Client/MessageWriter.cs
@@ -6,9 +6,9 @@ public static class MessageWriter
 {
     private const string _systemId = "System";
 
-    public static void WriteLine(IMessage message) => Console.WriteLine(BuildMessage(message.Message, message.Chat, message.Sender));
+    public static void WriteLine(IMessage message) => Console.WriteLine(BuildMessage(message.SentAt, message.Message, message.Chat, message.Sender));
 
-    public static void WriteSystemMessage(string message) => Console.WriteLine(BuildMessage(message, _systemId));
+    public static void WriteSystemMessage(string message) => Console.WriteLine(BuildMessage(DateTimeOffset.Now, message, _systemId));
 
-    private static string BuildMessage(string message, params object[] from) => $"[{string.Join('/', from)}] {message}";
+    private static string BuildMessage(DateTimeOffset time, string message, params object[] from) => $"[{time.ToLocalTime():HH:mm:ss} {string.Join('/', from)}] {message}";
 }
